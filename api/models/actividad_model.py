1019031695000a5462533dea2a8a57532599690d
Jaggo <jaggolap@hotmailcom>
2025-01-24 20:43:26 -0300

    Actualizado Actividad-listo

--- api/models/actividad_model.py
+++ api/models/actividad_model.py
@@ -7,8 +7,11 @@ class Actividad:
         self.nombre = kwargs.get('nombre')
         self.dias_habilitados = kwargs.get('dias_habilitados')
         self.horario = kwargs.get('horario')
+        self.sesiones_por_mes = kwargs.get('sesiones_por_mes')
         self.max_participantes = kwargs.get('max_participantes')
         self.participantes_actuales = kwargs.get('participantes_actuales', 0)
+        self.precio = kwargs.get('precio')
+        self.activo =kwargs.get('activo')
 
     def serialize(self):
         return {
@@ -16,8 +19,11 @@ class Actividad:
             'nombre': self.nombre,
             'dias_habilitados': self.dias_habilitados,
             'horario': self.horario.strftime('%H:%M:%S') if self.horario else None,
+            'sesiones_por_mes': self.sesiones_por_mes,
             'max_participantes': self.max_participantes,
-            'participantes_actuales': self.participantes_actuales
+            'participantes_actuales': self.participantes_actuales,
+            'precio': self.precio,
+            'activo': self.activo
         }
 
     @classmethod
@@ -26,18 +32,33 @@ class Actividad:
                    WHERE participantes_actuales < max_participantes"""
         results = DatabaseConnection.fetch_all(query)
         return [cls(**row) for row in results]
+        if result is not None:
+            return cls(
+                id=result[0],
+                nombre=result[1],
+                dias_habilitados=result[2],
+                horario=result[3],
+                sesiones_por_mes=result[4],
+                max_participantes=result[5],
+                participantes_actuales=result[6],
+                precio=result[7],
+                activo=result[8]
+            )
+        else:
+            return None
+
 
     @classmethod
     def create(cls, actividad):
-        query = """INSERT INTO gym_management.actividades (nombre, dias_habilitados, horario, max_participantes, participantes_actuales)
-                   VALUES (%s, %s, %s, %s, %s)"""
-        params = (actividad.nombre, actividad.dias_habilitados, actividad.horario, actividad.max_participantes, actividad.participantes_actuales)
+        query = """INSERT INTO gym_management.actividades (nombre, dias_habilitados, horario, sesiones_por_mes, max_participantes, participantes_actuales, precio, activo)
+                   VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"""
+        params = (actividad.nombre, actividad.dias_habilitados, actividad.horario, actividad.sesiones_por_mes, actividad.max_participantes, actividad.participantes_actuales, actividad.precio, actividad.activo)
         DatabaseConnection.execute_query(query, params=params)
 
     @classmethod
     def update(cls, actividad):
         query = """UPDATE gym_management.actividades
-                   SET nombre = %s, dias_habilitados = %s, horario = %s, max_participantes = %s, participantes_actuales = %s
+                   SET nombre = %s, dias_habilitados = %s, horario = %s, sesiones_por_mes = %s, max_participantes = %s, participantes_actuales = %s, precio = %s, activo = %s
                    WHERE id = %s"""
         params = (actividad.nombre, actividad.dias_habilitados, actividad.horario, actividad.max_participantes, actividad.participantes_actuales, actividad.id)
         DatabaseConnection.execute_query(query, params=params)
